6d182bbd810273ef80f273d44ad7019fa5219284
Tanner Perry <tc-boy@hotmail.com>
2021-10-04 15:52:35 -0600

    added graph

--- data_presenter.py
+++ data_presenter.py
@@ -1,5 +1,9 @@
 open_file = open('CupcakeInvoices.csv')
 store_total = 0
+vanilla_total = 0
+chocolate_total = 0
+strawberry_total = 0
+
 for value in open_file:
     value = value.rstrip('\n').split (',')
 
@@ -8,6 +12,13 @@ for value in open_file:
     flavor = value[2]
     quantity = int(value[3])
     price = float(value[4])
+
+    if flavor == 'Vanilla':
+        vanilla_total += 1
+    elif flavor == 'Chocolate':
+        chocolate_total += 1
+    else:
+        strawberry_total += 1
 
     customer_total = "{:.2f}".format(quantity * price)
 
@@ -19,29 +30,16 @@ for value in open_file:
  
     
 store_total = "{:.2f}".format(store_total)
-print(f"The store total profit is {store_total}")
+print(f"The store total profit is ${store_total}")
+print(f"{vanilla_total} Vanilla cupcakes were sold")
+print(f"{chocolate_total} Chocolate cupcakes were sold")
+print(f"{strawberry_total} Strawberry cupcakes were sold")
 
 open_file.close()
 
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-
-# labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-# men_means = [20, 35, 30, 35, 27]
-# women_means = [25, 32, 34, 20, 25]
-# men_std = [2, 3, 4, 1, 2]
-# women_std = [3, 5, 2, 3, 3]
-# width = 0.35       # the width of the bars: can also be len(x) sequence
-
-# fig, ax = plt.subplots()
-
-# ax.bar(labels, men_means, width, yerr=men_std, label='Men')
-# ax.bar(labels, women_means, width, yerr=women_std, bottom=men_means,
-#        label='Women')
-
-# ax.set_ylabel('Scores')
-# ax.set_title('Scores by group and gender')
-# ax.legend()
-
-# plt.show()
+data = [vanilla_total,chocolate_total,strawberry_total]
+plt.bar(['Vanilla','Chocolate','Strawberry'], data)
+plt.show()
